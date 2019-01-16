14a02f4e842a30189de4403eef02b6d221c07e8e
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2019-01-16 14:43:15 +0100

    Aufgabe5

--- src/Aufgabe5.java
+++ src/Aufgabe5.java
@@ -1,3 +1,5 @@
+import java.io.FileReader;
+
 public class Aufgabe5
 {
     public static void main (String[]args)
@@ -6,7 +8,17 @@ public class Aufgabe5
         // bei checked Exceptions überprüft der Compiler, ob diese entsprechend behandelt werden
         // unchecked Exceptions sind dem Java Compiler egal
 
-        //
 
+        // checked Beispiel
+       // Treten schon während der Kompilierung auf und werden rot markiert, FileNotFoundException
+
+        FileReader file = new FileReader("test");
+
+        // unchecked Beispiel
+        //Treten während der Laufzeit auf, ArrayIndexOutOfBoundsException
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println(arr[10]);
     }
 }
